fe226eddfeb8386e3718742018fe936f75a1f9ed
t_iwase <iwase2248@gmail.com>
2019-04-15 14:12:59 +0900

    練習問題3-3を追加しました。

---
+++ java003/java003-3/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int seibetsu = 0;
+		int age = 22;
+		System.out.println("こんにちわ");
+
+		if(seibetsu == 0) {
+			System.out.println("私は男です。" + age + "歳です。");
+		}else {
+			System.out.println("私は女です。");
+		}
+
+		System.out.println("よろしくお願いします。");
+
+	}
+
+}
