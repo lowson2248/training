749c9787d6bf0a01595446e0b5f08ea3669e768f
t_iwase <iwase2248@gmail.com>
2019-04-15 13:18:09 +0900

    masterからjava 1章 練習問題1-3を削除しました。

--- java001/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        boolean result = false;
-        char favoriteCharacter = '駆';
-        double pi = 3.14;
-        long number = 314159265853979L;
-        String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
-    }
-}
