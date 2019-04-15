a3ae647c9fa9130322ce32ce0e87c3dcb177a9a6
t_iwase <iwase2248@gmail.com>
2019-04-15 15:22:15 +0900

    練習問題4-4を追加

---
+++ java004-4/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numbers[]= {3,4,9};
+
+		System.out.println("一桁の数字を入力してください。");
+
+		int input = new java.util.Scanner(System.in).nextInt();
+
+		for(int num:numbers) {
+			if(num == input) {
+				System.out.println("あたり！");
+			}
+		}
+	}
+
+}
