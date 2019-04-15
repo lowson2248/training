4c89290ba39a0e0521541bd475e314d876811e03
t_iwase <iwase2248@gmail.com>
2019-04-15 15:24:33 +0900

    練習問題5-4を追加

---
+++ java005-4/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		double triangleArea = calcTriangleArea(10.0,5.0);
+		System.out.println("三角形の面積:" + triangleArea + "平方cm");
+
+		double circleArea = calcCircleArea(5.0);
+		System.out.println("円形の面積:" + circleArea + "平方cm");
+	}
+
+	private static double calcCircleArea(double radius) {
+		double area = radius*radius*3.14;
+		return area;
+	}
+
+	private static double calcTriangleArea(double bottom, double height) {
+		double area = (bottom * height)/2;
+		return area;
+	}
+
+
+
+}
